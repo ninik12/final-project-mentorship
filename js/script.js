4f63be43f0461d33d6983a983cf34d2c183d6ea6
Nino <123896230+ninik12@users.noreply.github.com>
2025-12-02 11:29:56 +0500

    add scroll to top arrow

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+// burger bar
 const burgerMenu = document.querySelector('.burger-menu');
 const navigation = document.getElementById('nav_menu')
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active')
     navigation.classList.toggle('activeNav')
-})
+});
+
+
+// scroll to top button
+const btn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 100){
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none"
+    }
+});
+
+btn.addEventListener("click", function(){
+    window.scrollTo({ top: 0, behavior: "smooth"})
+});
